1fe07fe0914b35a1aad55962a10ccd6cc306ee41
PG-8 (Nazwa) <pietroguiotto@gmail.com>
2020-05-25 18:00:02 +0200

    commit message

---
+++ pu.sh
@@ -0,0 +1,4 @@
+cd /home/pg-8/programming/shitty-ansa-scraper
+git add README.md
+git commit -m 'commit message'
+git push
--- script.sh
+++ script.sh
@@ -1,8 +1,3 @@
 curl --url 'https://www.ansa.it' >> /home/pg-8/programming/shitty-ansa-scraper/scrape.txt
 node /home/pg-8/programming/shitty-ansa-scraper/script.js
 > /home/pg-8/programming/shitty-ansa-scraper/scrape.txt
-
-cd /home/pg-8/programming/shitty-ansa-scraper
-git add README.md
-git commit -m 'commit message'
-git push
